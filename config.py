e3b63a5a4a9cea7ff0df9b4b5cdeefbbdd6ae2a5
Ian Dees <undees@gmail.com>
2010-06-01 07:25:20 -0700

    Commented example config file

--- config.py
+++ config.py
@@ -1,7 +1,13 @@
+# Where your to-be-displayed source code lives.
+#
 base = '~/src/snippetize'
 
+# Apple assigns these colors based on your style slide.
+#
 styles = {'Paragraph':373, 'Keyword':50, 'Operator':50,
           'Comment':53, 'Literal':51, 'Name.Function':52,
           'Name.Class':52}
 
+# Parts of files you wish to extract.
+#
 snippets = {'bar': ['def bar', 'pass']}
